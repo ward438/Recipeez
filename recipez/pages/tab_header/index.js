731ccaf13af661ec38e13686bae2c243fbbe62c7
ward438 <ward438@gmail.com>
2023-01-06 11:59:34 -0700

    resolving library issues

---
+++ recipez/pages/tab_header/index.js
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+export default function TabHeader() {
+  return (
+    <>   
+        <Box sx={{border: '1px solid white'}}>
+            <p>Tab header</p>
+        </Box>     
+    </>
+  )
+}
